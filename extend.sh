2b0dbf887ac43e57077d68e8c2d6a2a56598b4dd
Latecomessnow <hui.yumhappy@gmail.com>
2023-08-28 10:24:23 +0800

    shell

---
+++ extend.sh
@@ -0,0 +1,8 @@
+name="衣衣"
+str="你好，$name"
+str2="你好, ${name}"
+echo $str
+echo $str2
+echo ${#str}
+expr length "$str"
+echo ${str:1:10}
---
+++ name.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+name1='$DaMing\n'
+name2="LingLing\n"
+echo $name1
+echo $name2
+
+hello="i am $name1"
+echo $hello
+hi="i am $name2"
+echo $hi
